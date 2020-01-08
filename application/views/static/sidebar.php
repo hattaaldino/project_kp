c450940f25aa0c600a43ff77d1ae0ebffd51b13c
npnirwoko <56766923+npnirwoko@users.noreply.github.com>
2020-01-08 15:13:39 +0700

    Add files via upload

---
+++ application/views/static/sidebar.php
@@ -0,0 +1,21 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <!-- <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Orders
+            </a>
+        </ul>
+      </div> -->
+        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Project</a>
+            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+        </div>
+</nav>
