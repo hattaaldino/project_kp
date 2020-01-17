8b26b681e5c11cd4c88882324787ef579510aea4
npnirwoko <56766923+npnirwoko@users.noreply.github.com>
2020-01-17 13:44:13 +0700

    Add files via upload

---
+++ application/views/static/header.php
@@ -0,0 +1,29 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>Dashboard</title>
+
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/jumbotron/"> -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style_dashboard.css" >
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/font.css" >
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style1.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url();?>assets/js/index.js" ></script>
+  </head>
+
+  <body>
+  <nav class="navbar fixed-top bg-warnaaa flex-md-nowrap p-0 shadow ">
+      <a  href="#" ><img  src="<?php echo base_url();?>user_guide/_images/logo_main.png" height="35" width="125" class="ml-5"></a>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+        <a class="btn btn-primary btn-block pb-0 pt-0  " href="<?php echo base_url('loadview/logout'); ?>">Log Out</a>
+        </li>
+      </ul>
+    </nav>  
---
+++ application/views/static/sidebar_pengawas.php
@@ -0,0 +1,23 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <!-- <div class="nav flex-column nav-pills" id="v-pills-tab" aria-orientation="vertical" >
+        <ul class="nav flex-column">
+          <li class="nav-item">
+          <a class="nav-link active" data="pill" href="<?php echo base_url('contractor/dashboard_owner'); ?>">Proyek</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link " data="pill" href="<?php echo base_url('contractor/dashboard_profil'); ?>">profil</a>
+        </ul>
+      </div> -->
+
+
+        <!-- <div class="nav flex-column nav-pills" id="v-pills-tab" aria-orientation="vertical">
+            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" role="tab" aria-controls="v-pills-home" aria-selected="true" href="<?php echo base_url('contractor/dashboard_owner'); ?>">Project</a>
+            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" role="tab" aria-controls="v-pills-profile" aria-selected="false" href="<?php echo base_url('contractor/dashboard_profil'); ?>">Profile</a>
+        </div> -->
+
+
+        <div class="nav flex-column nav-pills">
+        <a class="nav-link " href="<?php echo base_url('contractor/dashboard_kontraktor'); ?>">Proyek</a>
+        <a class="nav-link " href="<?php echo base_url('contractor/dashboard_profil_kontraktor'); ?>">Profil</a>
+        </div>
+</nav>
